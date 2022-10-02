0b68bf5df351faa9eecd2d5b0c5e0ac430e7c85c
Kirtsun <olegkirtsun@gmail.com>
2022-10-02 16:25:49 +0300

    branch one, parse

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 def parse(query: str) -> dict:
+    dict_one = {}
     a = "?"
-
-    return {}
-
+    b = query.find(a) + 1
+    query = query[b:]
+    query = query.replace('=', ' ').replace('&', ' ')
+    query = query.split()
+    if len(query) == 0:
+        return dict_one
+    if b == 0:
+        return dict_one
+    key = query[::2]
+    value = query[1::2]
+    dict_one = {key[i]: value[i] for i in range(len(key))}
+    return dict_one
 
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
@@ -12,12 +22,12 @@ if __name__ == '__main__':
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
 
-def parse_cookie(query: str) -> dict:
-    return {}
-
-
-if __name__ == '__main__':
-    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookie('') == {}
-    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+# def parse_cookie(query: str) -> dict:
+#     return {}
+#
+#
+# if __name__ == '__main__':
+#     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+#     assert parse_cookie('') == {}
+#     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+#     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
